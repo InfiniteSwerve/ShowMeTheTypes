39ca17f97c219c2e74d18b968d2f3ba4b835379c
InfiniteSwerve <jackofthestrand@gmail.com>
2023-11-07 14:45:00 -0800

    feature: allow custom handlers

--- showmethetypes/core.py
+++ showmethetypes/core.py
@@ -2,15 +2,17 @@ from showmethetypes.registry import (
     get_handler,
     get_registry,
     register_handlers,
+    register_handler,
 )
 from showmethetypes.handlers import check_storage
+from typing import Optional, Any
 
 
 class SMTT:
-    # TODO: imported_handlers can be strings, need an argumnent for user defined handlers
     def __init__(
         self,
         *imported_handlers,
+        custom_handlers: Optional[list[Any]] = None,
     ):
         import sys
         import types
@@ -30,18 +32,21 @@ class SMTT:
         if imported_handlers != ():
             for storage in imported_handlers:
                 register_handlers(storage)
+        if custom_handlers != None:
+            for name, handler in custom_handlers:
+                register_handler(name, handler)
 
     # TODO: Be able to pass in "monomorphic" = True to make default behavior find all unique lengths..
-    def __call__(self, obj):
+    def __call__(self, obj) -> None:
         self.lines = []
         self.traverse(obj)
         self.display()
 
-    def traverse(self, thing, indent=0, is_last=True, prefix=""):
+    def traverse(self, thing, indent=0, is_last=True, prefix="") -> None:
         # Determine the type of the current object
         handler = get_handler(type(thing).__name__.lower()) or get_handler("default")
         handler(self, thing, indent, is_last, prefix)
 
-    def display(self):
+    def display(self) -> None:
         for line in self.lines:
             print(line)
